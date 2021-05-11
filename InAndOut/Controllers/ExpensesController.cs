36278e3d1ce8a33820c52c8692ac15dc1181b04f
Jamal Hussein <jamal.hussein@tietoevry.com>
2021-05-11 18:27:07 +0200

    update expenses implemented

--- InAndOut/Controllers/ExpensesController.cs
+++ InAndOut/Controllers/ExpensesController.cs
@@ -64,5 +64,32 @@ namespace InAndOut.Controllers
             _context.SaveChanges();
             return RedirectToAction("Index");
         }
+
+        //GET UPDATE
+        public IActionResult Update(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var expense = _context.Expenses.Find(id);
+            if (expense == null) return NotFound();
+            return View(expense);
+
+        }
+
+        // POST UPDATE
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult UpdatePost(Expense model)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Update(model);
+                _context.SaveChanges();
+            }
+            
+            return RedirectToAction("Index");
+        }
     }
 }
