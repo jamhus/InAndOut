e4f3fe75f807dc0a273fded5602c6f7c8c1aeb17
Jamal Hussein <jamal.hussein@tietoevry.com>
2021-05-11 14:43:17 +0200

    update model Items

--- InAndOut/Migrations/20210511123045_updateItemModel.Designer.cs
+++ InAndOut/Migrations/20210511123045_updateItemModel.Designer.cs
@@ -3,14 +3,16 @@ using InAndOut.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace InAndOut.Migrations
 {
     [DbContext(typeof(DataContext))]
-    partial class DataContextModelSnapshot : ModelSnapshot
+    [Migration("20210511123045_updateItemModel")]
+    partial class updateItemModel
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
