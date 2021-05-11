11de11c86f9b2c3ca486646119cddb2bbc2a1440
Jamal Hussein <jamal.hussein@tietoevry.com>
2021-05-11 19:04:11 +0200

    added migration for ex types and updated db

--- InAndOut/Migrations/20210511165528_AddedExpenseType.Designer.cs
+++ InAndOut/Migrations/20210511165528_AddedExpenseType.Designer.cs
@@ -3,14 +3,16 @@ using InAndOut.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace InAndOut.Migrations
 {
     [DbContext(typeof(DataContext))]
-    partial class DataContextModelSnapshot : ModelSnapshot
+    [Migration("20210511165528_AddedExpenseType")]
+    partial class AddedExpenseType
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
